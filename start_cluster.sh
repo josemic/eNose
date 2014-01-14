f3d80963e0db8c0ab34840302a81ae5cd458e17b
M. Josenhans <m_josenhans@web.de>
2014-01-14 17:12:50 +0100

    Removed warning level, added lager start and reading of app.config

--- start_cluster.sh
+++ start_cluster.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo $@
 echo $1 $2
-erl +W i -sname enose_$2_$1 -setcookie myCookie -boot start_sasl -pa $PWD/examples/ebin $PWD/rule/ebin $PWD/deps/*/ebin $PWD/content_app/ebin $PWD/stream_app/ebin $PWD/epcap_port_app/ebin -s s12_PF_RING_cluster s $1 $2
+# use erl option "+w i" to e.g. set the warning level to info
+erl -sname enose_$2_$1 -setcookie myCookie -boot start_sasl -pa $PWD/examples/ebin $PWD/rule/ebin $PWD/deps/*/ebin $PWD/content_app/ebin $PWD/stream_app/ebin $PWD/epcap_port_app/ebin -s s12_PF_RING_cluster s $1 $2 -s lager -config app.config
 
